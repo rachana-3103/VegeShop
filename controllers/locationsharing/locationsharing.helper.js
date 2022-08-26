90b159feb5dddb440ba97f15112adf63c1d8a511
rachana-3103 <rachanaprajapati1234@gmail.com>
2022-08-26 17:20:52 +0530

    changes in alert api

--- controllers/locationsharing/locationsharing.helper.js
+++ controllers/locationsharing/locationsharing.helper.js
@@ -1,6 +1,12 @@
 const { locationsharings } = require("../../models/index");
 const axios = require("axios");
 const uuid = require("uuid");
+const AWS = require("aws-sdk");
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: process.env.AWS_REGION,
+});
 const sns = new AWS.SNS();
 
 exports.locationSharing = async (param) => {
