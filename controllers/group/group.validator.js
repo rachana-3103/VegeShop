d5215de2603178616ba41123250c1ea6e651595c
ashvin <ashvin.kumbhani1@bacancy.com>
2022-09-23 16:31:25 +0530

    changes in contact responded api

--- controllers/group/group.validator.js
+++ controllers/group/group.validator.js
@@ -5,7 +5,7 @@ const { validateRequest } = require("../../helpers/helpers");
 exports.addGroup = async (req, res, next) => {
   const param = { ...req.body };
   const schema = Joi.object({
-    name: Joi.string().alphanum().max(30).required(),
+    name: Joi.string().alphanum().max(20).required(),
     contacts: Joi.array().required(),
   });
 
