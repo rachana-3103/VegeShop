4a512b6335b06eba9c67eb3c8c2e5f13c40740b7
ashvin <ashvin.kumbhani1@bacancy.com>
2022-11-16 11:20:46 +0530

    sms changes

--- controllers/group/group.validator.js
+++ controllers/group/group.validator.js
@@ -5,7 +5,7 @@ const { validateRequest } = require("../../helpers/helpers");
 exports.addGroup = async (req, res, next) => {
   const param = { ...req.body };
   const schema = Joi.object({
-    name: Joi.string().max(20).required(),
+    name: Joi.string().max(30).required(),
     contacts: Joi.array().required(),
   });
 
@@ -21,7 +21,7 @@ exports.updateGroup = async (req, res, next) => {
   const param = { ...req.body };
   const schema = Joi.object({
     groupId:Joi.string().required(),
-    name: Joi.string().alphanum().max(30).required(),
+    name: Joi.string().max(30).required(),
     contacts: Joi.array().required(),
   });
 
