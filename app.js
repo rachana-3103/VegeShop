0f28ded8cf8cc408e7827de931561f61eb2380f2
ashvin <ashvin.kumbhani1@bacancy.com>
2022-11-22 14:39:46 +0530

    live location sms changes

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const logger = require("morgan");
 const cors = require("cors");
 const rateLimit = require("express-rate-limit");
 const path = require("path");
-const router = express.Router();
 const userRoutes = require("./routes/user.routes");
 const groupRoutes = require("./routes/group.routes");
 const safetyPlanRoutes = require("./routes/safetyplan.routes");
 const locationRoutes = require("./routes/location.routes");
 const locationSharingRoutes = require("./routes/locationsharing.routes");
 const faqRoutes = require("./routes/faq.routes");
-const locationSharingController = require("./controllers/locationsharing/locationsharing.controller");
 
 const { errorHandler } = require("./middleware/errorHandler");
 const { TOO_MANY_REQUESTS } = require("./helpers/messages");
@@ -35,12 +33,12 @@ app.use("/documents", express.static("documents"));
 
 //set view engine
 app.set("view engine", "ejs");
-app.use("/api/user", apiLimiter, userRoutes);
-app.use("/api/group", apiLimiter, groupRoutes);
-app.use("/api/safetyplan", apiLimiter, safetyPlanRoutes);
-app.use("/api/location", apiLimiter, locationRoutes);
-app.use("/api/location-sharing", apiLimiter, locationSharingRoutes);
-app.use("/api/faqs", apiLimiter, faqRoutes);
+app.use("/api/v1/user", apiLimiter, userRoutes);
+app.use("/api/v1/group", apiLimiter, groupRoutes);
+app.use("/api/v1/safetyplan", apiLimiter, safetyPlanRoutes);
+app.use("/api/v1/location", apiLimiter, locationRoutes);
+app.use("/api/v1/location-sharing", apiLimiter, locationSharingRoutes);
+app.use("/api/v1/faqs", apiLimiter, faqRoutes);
 
 // router.post(
 //   `https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=${process.env.KEY}`,
