8711b0ff8294b87a3658f89914a8259546c10efb
ashvin <ashvin.kumbhani1@bacancy.com>
2022-11-25 14:29:02 +0530

    changes in migration

--- migrations/20220818100824-modify-safetyplan.js
+++ migrations/20220818100824-modify-safetyplan.js
@@ -1,19 +1,19 @@
 "use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // queryInterface.addColumn("safetyplans", "location_id", {
-    //   type: Sequelize.STRING,
-    //   allowNull: true,
-    // }),
-      // queryInterface.addColumn("locationsharings", "current_latitude", {
-      //   type: Sequelize.STRING,
-      // }),
-      // queryInterface.addColumn("locationsharings", "current_longitude", {
-      //   type: Sequelize.STRING,
-      // }),
-      // queryInterface.addColumn("locationsharings", "destination_latitude", {
-      //   type: Sequelize.STRING,
-      // }),
+    queryInterface.addColumn("safetyplans", "location_id", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    }),
+      queryInterface.addColumn("locationsharings", "current_latitude", {
+        type: Sequelize.STRING,
+      }),
+      queryInterface.addColumn("locationsharings", "current_longitude", {
+        type: Sequelize.STRING,
+      }),
+      queryInterface.addColumn("locationsharings", "destination_latitude", {
+        type: Sequelize.STRING,
+      }),
       queryInterface.addColumn("locationsharings", "destination_longitude", {
         type: Sequelize.STRING,
       }),
@@ -33,10 +33,10 @@ module.exports = {
       })
   },
   down: async (queryInterface, Sequelize) => {
-    // queryInterface.addColumn("safetyplans", "location_id");
-    // queryInterface.addColumn("locationsharings", "current_latitude");
-    // queryInterface.addColumn("locationsharings", "current_longitude");
-    // queryInterface.addColumn("locationsharings", "destination_latitude");
+    queryInterface.addColumn("safetyplans", "location_id");
+    queryInterface.addColumn("locationsharings", "current_latitude");
+    queryInterface.addColumn("locationsharings", "current_longitude");
+    queryInterface.addColumn("locationsharings", "destination_latitude");
     queryInterface.addColumn("locationsharings", "destination_longitude");
     queryInterface.removeColumn("locationsharings", "latitude");
     queryInterface.removeColumn("locationsharings", "longitude");
