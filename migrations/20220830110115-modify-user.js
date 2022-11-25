88fdb5cb72eca48e29221bb6b0857faea78044f5
ashvin <ashvin.kumbhani1@bacancy.com>
2022-11-25 14:26:09 +0530

    changes in migration

--- migrations/20220830110115-modify-user.js
+++ migrations/20220830110115-modify-user.js
@@ -1,21 +1,21 @@
 "use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    queryInterface.addColumn("users", "notification", {
-      type: Sequelize.BOOLEAN,
-      defaultValue: true,
-    }),
-      queryInterface.addColumn("locationsharings", "status", {
-        type: Sequelize.STRING,
-      }),
-      queryInterface.addColumn("users", "battery", {
-        type: Sequelize.STRING,
-      });
+    // queryInterface.addColumn("users", "notification", {
+    //   type: Sequelize.BOOLEAN,
+    //   defaultValue: true,
+    // }),
+    //   queryInterface.addColumn("locationsharings", "status", {
+    //     type: Sequelize.STRING,
+    //   }),
+    //   queryInterface.addColumn("users", "battery", {
+    //     type: Sequelize.STRING,
+    //   });
   },
   down: async (queryInterface, Sequelize) => {
-    queryInterface.removeColumn("users", "notification");
-    queryInterface.removeColumn("locationsharings", "status");
-    queryInterface.removeColumn("users", "battery");
+    // queryInterface.removeColumn("users", "notification");
+    // queryInterface.removeColumn("locationsharings", "status");
+    // queryInterface.removeColumn("users", "battery");
 
   },
 };
